584f103ad060c9f4109f1a9f99c9d97c1991f467
unknown <lukasz.mikolaj.gronowski@gmail.com>
2017-03-02 18:21:32 +0100

    Add some json and jackson exercises.

---
+++ src/main/java/pl/sda/json/Main.java
@@ -0,0 +1,24 @@
+package pl.sda.json;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * Created by RENT on 2017-03-02.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        User user1 = new User();
+        user1.setFirstName("Jan");
+        user1.setLastName("Kowalski");
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        String json =  mapper.writeValueAsString(user1);
+
+        System.out.println(json);
+
+    }
+}
